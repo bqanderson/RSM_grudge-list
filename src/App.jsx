581be7b5e7dee5f326d237c58593d417f5051754
Brian Q Anderson <bqanderson@me.com>
2020-04-23 09:07:06 -0500

    Create UNDO reducer

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
-import React from 'react'
+import React, { useContext } from 'react'
 
 import Grudges from './Grudges'
 import NewGrudge from './NewGrudge'
+import { GrudgeContext } from './GrudgeContext'
 
 const App = () => {
+  const { undo, isPast } = useContext(GrudgeContext)
+
   return (
     <div className='Application'>
       <NewGrudge />
+      <section style={{ marginTop: '10px' }}>
+        <button disabled={!isPast} onClick={undo}>
+          Undo
+        </button>
+        <button>Redo</button>
+      </section>
       <Grudges />
     </div>
   )
